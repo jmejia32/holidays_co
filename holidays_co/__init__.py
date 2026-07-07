02141fc3eca2de899f6a72cc1de6487dab9fc0d2
Javier Mejía Estrada <jmejia@energiasolarsa.com>
2026-07-07 14:34:34 -0500

    reimplementation for v1.1
    - dataclass instead of namedtuple
    - type hints

--- holidays_co/__init__.py
+++ holidays_co/__init__.py
@@ -1,40 +1,54 @@
 import calendar
-from collections import namedtuple
+from collections import defaultdict
+from dataclasses import dataclass
 from datetime import date, timedelta, datetime
-_nt_holiday_stock = namedtuple("Holiday", ["day", "days_to_sum", "celebration"])
+from functools import reduce
 
-EASTER_WEEK_HOLIDAYS = [
-    _nt_holiday_stock(day=-3, days_to_sum=None, celebration="Jueves Santo"),
-    _nt_holiday_stock(day=-2, days_to_sum=None, celebration="Viernes Santo"),
-    _nt_holiday_stock(day=39, days_to_sum=calendar.MONDAY, celebration="Ascensión del Señor"),
-    _nt_holiday_stock(day=60, days_to_sum=calendar.MONDAY, celebration="Corphus Christi"),
-    _nt_holiday_stock(day=68, days_to_sum=calendar.MONDAY, celebration="Sagrado Corazón de Jesús")
-]
+@dataclass
+class HolidayStock:
+    celebration: str
+    day: int = None
+    month: int = None
+    day_delta: int = None
+    move_to: calendar.Day = None
 
-HOLIDAYS = [
-    _nt_holiday_stock(day="01-01", days_to_sum=None, celebration="Año Nuevo"),
-    _nt_holiday_stock(day="05-01", days_to_sum=None, celebration="Día del Trabajo"),
-    _nt_holiday_stock(day="07-20", days_to_sum=None, celebration="Día de la Independencia"),
-    _nt_holiday_stock(day="08-07", days_to_sum=None, celebration="Batalla de Boyacá"),
-    _nt_holiday_stock(day="12-08", days_to_sum=None, celebration="Día de la Inmaculada Concepción"),
-    _nt_holiday_stock(day="12-25", days_to_sum=None, celebration="Día de Navidad"),
-    _nt_holiday_stock(day="01-06", days_to_sum=calendar.MONDAY, celebration="Día de los Reyes Magos"),
-    _nt_holiday_stock(day="03-19", days_to_sum=calendar.MONDAY, celebration="Día de San José"),
-    _nt_holiday_stock(day="06-29", days_to_sum=calendar.MONDAY, celebration="San Pedro y San Pablo"),
-    _nt_holiday_stock(day="08-15", days_to_sum=calendar.MONDAY, celebration="La Asunción de la Virgen"),
-    _nt_holiday_stock(day="10-12", days_to_sum=calendar.MONDAY, celebration="Día de la Raza"),
-    _nt_holiday_stock(day="11-01", days_to_sum=calendar.MONDAY, celebration="Todos los Santos"),
-    _nt_holiday_stock(day="11-11", days_to_sum=calendar.MONDAY, celebration="Independencia de Cartagena")
-]
+@dataclass
+class Holiday:
+    date: date
+    celebration: str
 
-def next_weekday(d, weekday):
+EASTER_WEEK_HOLIDAYS: tuple[HolidayStock] = (
+    HolidayStock(day_delta=-3, move_to=None, celebration="Jueves Santo"),
+    HolidayStock(day_delta=-2, move_to=None, celebration="Viernes Santo"),
+    HolidayStock(day_delta=40, move_to=calendar.MONDAY, celebration="Ascensión del Señor"),
+    HolidayStock(day_delta=60, move_to=calendar.MONDAY, celebration="Corphus Christi"),
+    HolidayStock(day_delta=67, move_to=calendar.MONDAY, celebration="Sagrado Corazón de Jesús")
+)
+
+HOLIDAYS: tuple[HolidayStock] = (
+    HolidayStock(month=1, day=1, move_to=None, celebration="Año Nuevo"),
+    HolidayStock(month=1, day=6, move_to=calendar.MONDAY, celebration="Día de los Reyes Magos"),
+    HolidayStock(month=3, day=19, move_to=calendar.MONDAY, celebration="Día de San José"),
+    HolidayStock(month=5, day=1, move_to=None, celebration="Día del Trabajo"),
+    HolidayStock(month=6, day=29, move_to=calendar.MONDAY, celebration="San Pedro y San Pablo"),
+    HolidayStock(month=7, day=20, move_to=None, celebration="Día de la Independencia"),
+    HolidayStock(month=8, day=7, move_to=None, celebration="Batalla de Boyacá"),
+    HolidayStock(month=8, day=15, move_to=calendar.MONDAY, celebration="La Asunción de la Virgen"),
+    HolidayStock(month=10, day=12, move_to=calendar.MONDAY, celebration="Día de la Raza"),
+    HolidayStock(month=11, day=1, move_to=calendar.MONDAY, celebration="Todos los Santos"),
+    HolidayStock(month=11, day=11, move_to=calendar.MONDAY, celebration="Independencia de Cartagena"),
+    HolidayStock(month=12, day=8, move_to=None, celebration="Día de la Inmaculada Concepción"),
+    HolidayStock(month=12, day=25, move_to=None, celebration="Día de Navidad")
+)
+
+def _next_weekday(d: date, weekday: int) -> date:
     """ https://stackoverflow.com/a/6558571 """
     days_ahead = weekday - d.weekday()
     if days_ahead <= 0: # Target day already happened this week
         days_ahead += 7
     return d + timedelta(days_ahead)
 
-def calc_easter(year):
+def _calc_easter(year: int) -> date:
     """ Returns Easter as a date object.
 
     upstream: http://code.activestate.com/recipes/576517-calculate-easter-western-given-a-year/
@@ -55,7 +69,42 @@ def calc_easter(year):
     day = f % 31 + 1
     return date(year, month, day)
 
-def get_colombia_holidays_by_year(year):
+def _holidays_by_month(year: int) -> dict[int, list[Holiday]]:
+    result = defaultdict(list)
+    for holiday in HOLIDAYS:
+        holiday_date = date(year, holiday.month, holiday.day)
+        if holiday.move_to is not None and holiday_date.weekday() != holiday.move_to.value:
+            holiday_date = _next_weekday(holiday_date, holiday.move_to.value)
+
+        key = holiday_date.month
+        result[key].append(
+            Holiday(date=holiday_date, celebration=holiday.celebration)
+        )
+
+        if len(result[key]) < 2:
+            continue
+
+        result[key].sort(key=lambda holiday: holiday.date)
+
+    sunday_date = _calc_easter(year)
+    for holiday in EASTER_WEEK_HOLIDAYS:
+        holiday_date = sunday_date + timedelta(days=holiday.day_delta)
+        if holiday.move_to is not None and holiday_date.weekday() != holiday.move_to:
+            holiday_date = _next_weekday(holiday_date, holiday.move_to.value)
+
+        key = holiday_date.month
+        result[key].append(
+            Holiday(date=holiday_date, celebration=holiday.celebration)
+        )
+
+        if len(result[key]) < 2:
+            continue
+
+        result[key].sort(key=lambda holiday: holiday.date)
+
+    return dict(result)
+
+def get_colombia_holidays_by_year(year: int) -> list[Holiday]:
     try:
         year = int(year)
     except ValueError:
@@ -64,30 +113,20 @@ def get_colombia_holidays_by_year(year):
     if year < 1970 or year > 99999:
         raise ValueError("El año debe ser mayor a 1969 y menor a 100000")
 
-    nt_holiday = namedtuple("Holiday", ["date", "celebration"])
-    normal_holidays = []
-    for holiday in HOLIDAYS:
-        holiday_date = datetime.strptime("%s-%d" % (holiday.day, year), "%m-%d-%Y").date()
-        if holiday.days_to_sum is not None and holiday_date.weekday() != holiday.days_to_sum:
-            holiday_date = next_weekday(holiday_date, holiday.days_to_sum)
-        normal_holidays.append(nt_holiday(date=holiday_date, celebration=holiday.celebration))
+    holiday_d = _holidays_by_month(year)
+    keys = sorted(holiday_d.keys())
+    return list(
+        reduce(lambda result, k: result + holiday_d[k], keys, [])
+    )
 
-    sunday_date = calc_easter(year)
-    easter_holidays = []
-    for holiday in EASTER_WEEK_HOLIDAYS:
-        holiday_date = sunday_date + timedelta(days=holiday.day)
-        if holiday.days_to_sum is not None and holiday_date.weekday() != holiday.days_to_sum:
-            holiday_date = next_weekday(holiday_date, holiday.days_to_sum)
-        easter_holidays.append(nt_holiday(date=holiday_date, celebration=holiday.celebration))
-
-    holiday_list = normal_holidays + easter_holidays
-    holiday_list.sort(key=lambda holiday: holiday.date)
-    return holiday_list
-
-def is_holiday_date(d):
+def is_holiday_date(d: datetime | date):
     if not isinstance(d, date):
         raise TypeError("Debe proporcionar un objeto tipo date")
     if isinstance(d, datetime):
         d = d.date()
-    holiday_list = set([holiday.date for holiday in get_colombia_holidays_by_year(d.year)])
-    return d in holiday_list
+
+    holiday_d = _holidays_by_month(d.year)
+    if d.month not in holiday_d:
+        return False
+
+    return next((True for i in holiday_d[d.month] if i.date == d), False)
